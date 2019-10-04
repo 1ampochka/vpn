844d2b5c0095a1bbeb6c50e09591f0771a731210
1ampochka <37929434+1ampochka@users.noreply.github.com>
2019-10-04 18:31:40 +0500

    Add files via upload
    добавлена рестарт впн в скрипт, вырезака из пинга время ответа от сервера,

--- restartopenvpn.sh
+++ restartopenvpn.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #пингует хост 195.123.221.109 каждые 10 секунд и при необходимости перезапускает openvpn
-#while [[ varping -lt 1000 ]]
-#do 	
-	datetime=$(zdump +5)
-	varping=$(ping -c 1 195.123.221.109 | cut -d= -f4) 
-	varping=$varping | grep data.
-	echo $varping
-	#echo "$datetime $varping"
-	#sleep 9
-#done
 
-#test=$(ls)
-#echo $test'
+while [[ varping -lt 1000 ]]
+do 	
+	datetime=$(zdump +5)
+	#вырезает из вывода пинга время эхо пакета
+	varping=$(ping -c 1 195.123.221.109 | grep time | cut -d ' ' -f 7 | cut -d= -f 2)
+	varping=${varping//[^0-9]/}
+	#вывод в файл время ответа и ожидает 10 секунд
+	echo "$datetime ping $varping times"
+	sleep 10
+done
+
+systemctl restart openvpn@vpn_lampochka.ovpn
+
